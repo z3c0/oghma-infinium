38f2e96985643a62787c01d9afb2d32cd0d7b2a0
z3c0 <z3c0@21337.tech>
2022-03-09 20:31:48 -0500

    add shebang

--- install-geckodriver-linux.sh
+++ install-geckodriver-linux.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux32.tar.gz
 tar -xf geckodriver-v0.30.0-linux32.tar.gz
 mv geckodriver /usr/local/bin
